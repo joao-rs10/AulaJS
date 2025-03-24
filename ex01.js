cad2391b8796722a995e04630b9fe27262eae533
João Vitor <joaovitorreis1000@gmail.com>
2025-03-24 10:19:33 -0300

    aulasJS

---
+++ ex01.js
@@ -0,0 +1,10 @@
+var nome = "joao";
+var sobrenome = " vitor";
+var ano = 2025;
+var idade = 19;
+
+var nomeSobrenome = nome + sobrenome;
+var nascimento = ano - idade;
+
+console.log(nomeSobrenome);
+console.log(nascimento);
---
+++ main.js
@@ -0,0 +1,46 @@
+// comentario de uma linha
+/*
+    comentario de varias linhas
+*/
+
+var cliente = 'Maria;'
+var quantidade = 30;
+var preco = 50.50;
+var emEstoque = true;
+
+var numeroA = 1;
+var numeroB = 2;
+var numeroC = 3;
+
+var resultadoSoma = numeroA + numeroB;
+var resultadoSubtracao = numeroB - numeroA;
+var resultadoProduto = numeroB * numeroC;
+var resultadoDivisao = numeroC / numeroB;
+
+console.log(resultadoSoma);
+console.log(resultadoSubtracao);
+console.log(resultadoProduto);
+console.log(resultadoDivisao);
+
+var textoA = 'web';
+var textoB = 'dev';
+var textoC = ' ';
+
+var resultadoA = textoA + textoB; //webdev
+var resultadoB= textoB + 1; //dev1
+var resultadoC= textoA+ textoC + textoB; //web  dev
+
+console.log(resultadoA);
+console.log(resultadoB);
+console.log(resultadoC);
+
+// inicio aula03
+
+var nomeInserido = prompt('Insira seu nome e sobrenome: ');
+console.log("O usuário " + nomeInserido + " logou no site");
+
+alert("Olá " + nomeInserido + " bem-vindo!");
+var idadeInserida = prompt("Insira sua idade: ");
+var idadeCorreta = parseInt(idadeInserida);
+
+alert("Sua idade é " + idadeCorreta);git 
